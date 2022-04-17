56838ed1ea08e8c474a9b29842b5144d9c82fa3f
thoringandalf <95851889+thoringandalf@users.noreply.github.com>
2022-04-17 10:34:55 -0700

    Update mgmt_general.js

--- mgmt_general.js
+++ mgmt_general.js
@@ -588,17 +588,5 @@ let fbConfig = {
   
   /*** *** ON PAGE LOAD-UP *** ***/
   
-  initializeDateVars();
-  populateCalendarHeading();
-  populateDateCards();
-  switchSidebarTabs(0);
-  fillToggles();
-  renderMainRequests();
-  renderExternalRequests();
-  initializeDayBoxes();
-  initializeBoatBoxes();
-  initializePrices();
-  toggleDays(false);
-  toggleBoats(false);
-  togglePrices(false);
+
   
