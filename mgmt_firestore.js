8c7f38564f49a2b3d155e458c6fe4e2a07f0b001
thoringandalf <95851889+thoringandalf@users.noreply.github.com>
2022-04-17 11:06:46 -0700

    Update mgmt_firestore.js

--- mgmt_firestore.js
+++ mgmt_firestore.js
@@ -1,1 +1,15 @@
+/*** *** ON PAGE LOAD-UP *** ***/
 
+ initializeDateVars();
+ populateCalendarHeading();
+ populateDateCards();
+ switchSidebarTabs(0);
+ fillToggles();
+ renderMainRequests();
+ renderExternalRequests();
+ initializeDayBoxes();
+ initializeBoatBoxes();
+ initializePrices();
+ toggleDays(false);
+ toggleBoats(false);
+ togglePrices(false);
